6fd8cd5dc81ae2e869136352ebb9b0f81dee39d4
VictorManduca <victor.manduca@hotmail.com>
2020-01-16 01:21:19 -0300

    Implementing front-end with ReactJS

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 
@@ -11,6 +12,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-vpqt4.mongodb.net/w
   useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
